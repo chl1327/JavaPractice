2713db97d5ee60c295e37a5611304079c1ba197e
rong.chen <357929486@qq.com>
2018-08-08 21:27:41 +0800

    TKJavaU2

---
+++ src/com/java/TkJava/Test.java
@@ -0,0 +1,19 @@
+package com.java.TkJava;
+
+import com.java.TkJava.TkU2.Practise;
+
+/**
+ * Created by chl1327 on 2018/8/8.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        Practise practise = new Practise();
+        Practise practise1 = new Practise();
+        System.out.println(practise1.toString());
+        System.out.println(practise.toString());
+        practise.increment();
+        System.out.println(practise1.toString());
+        System.out.println(practise.toString());
+    }
+}
---
+++ src/com/java/TkJava/TkU2/Practise.java
@@ -0,0 +1,27 @@
+package com.java.TkJava.TkU2;
+
+/**
+ * Created by chl1327 on 2018/8/8.
+ */
+public class Practise {
+
+    int i;
+
+    char a;
+
+    static int k= 47;
+
+    @Override
+    public String toString() {
+        return "Practise{" +
+                "i=" + i +
+                ", a=" + a +
+                ", k=" + k +
+                '}';
+    }
+
+    public static void increment(){
+        Practise.k++;
+    }
+
+}
