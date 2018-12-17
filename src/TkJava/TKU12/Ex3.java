4d73aebd4b0507f94cf929987807a03b9740f90c
chl1327 <357929486@qq.com>
2018-12-17 21:12:35 +0800

    TKU12

---
+++ src/TkJava/TKU12/Ex3.java
@@ -0,0 +1,25 @@
+package TkJava.TKU12;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+public class Ex3 {
+    private static int[] ia = new int[2];
+    private static Logger logger = Logger.getLogger("LoggingException");
+    static void logException(Exception e) { // Exception e argument
+        StringWriter trace = new StringWriter();
+        e.printStackTrace(new PrintWriter(trace));
+        logger.severe(trace.toString());
+    }
+    public static void main(String[] args) {
+        try {
+            ia[2] = 3;
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.err.println(
+                    "Caught ArrayIndexOutOfBoundsException");
+            e.printStackTrace();
+            logException(e);
+        }
+    }
+}
