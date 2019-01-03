e6ab82bd9c6e836989f672a513b0a7d9a755efe7
chl1327 <357929486@qq.com>
2019-01-03 20:33:27 +0800

    TKU13

---
+++ src/TkJava/TKU13/InfiniteRecursion2.java
@@ -0,0 +1,16 @@
+package TkJava.TKU13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfiniteRecursion2 {
+    public String toString() {
+        return " InfiniteRecursion address: " + super.toString() + "\n";
+    }
+    public static void main(String[] args) {
+        List<InfiniteRecursion2> v = new ArrayList<InfiniteRecursion2>();
+        for(int i = 0; i < 10; i++)
+            v.add(new InfiniteRecursion2());
+        System.out.println(v);
+    }
+}
