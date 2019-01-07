c2888154d1f0bc715e1105abe1e9a65e6f8a9b73
chl1327 <357929486@qq.com>
2019-01-07 21:39:24 +0800

    TKU13

---
+++ src/TkJava/TKU13/Ex7.java
@@ -0,0 +1,10 @@
+package TkJava.TKU13;
+
+import java.util.Arrays;
+
+public class Ex7 {
+
+    public static void main (String[] args){
+        System.out.println("Then.".matches("^[A-Z].*[\\.]$"));
+    }
+}
---
+++ src/TkJava/TKU13/Splitting.java
@@ -0,0 +1,18 @@
+package TkJava.TKU13;
+
+import java.util.Arrays;
+
+public class Splitting {
+    public static String knights =
+            "Then, when you have found the shrubbery, you must " +
+                    "cut down the mightiest tree in the forest... " +
+                    "with... a herring!";
+    public static void split(String regex) {
+        System.out.println(Arrays.toString(knights.split(regex)));
+    }
+
+    public static void main(String[] args) {
+        //split("the|you");
+        System.out.println(knights.replaceAll("[aeiouAEIOU]","_"));
+    }
+}
