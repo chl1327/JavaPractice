023c2fdca9d14eecab8e46fcd1c8fd22a55ff44a
rong.chen <357929486@qq.com>
2019-04-15 17:57:54 +0800

    TK20 2

---
+++ src/TkJava/TKU20/extract/Divider.java
@@ -0,0 +1,33 @@
+package TkJava.TKU20.extract;
+
+import static print.Print.print;
+
+@ExtractInterface("IDivider")
+public class Divider {
+    public int divide(int x, int y) throws ArithmeticException {
+        if(y == 0) throw new ArithmeticException("Divide by zero");
+        int diff = subtract(x, y);
+        if(diff < 0) return 0;
+        if(diff == 0) return 1;
+        int result = 0;
+        while(diff >= 0) {
+            result++;
+            diff = subtract(diff, y);
+        }
+        return result;
+    }
+    private int subtract(int x, int y) { return x - y; }
+    public static void main(String[] args) {
+        try {
+            Divider d = new Divider();
+            print("11/16 = " + d.divide(11, 16));
+            print("10/10 = " + d.divide(10,10));
+            print("10/1 = " + d.divide(10,1));
+            print("10/2 = " + d.divide(10,2));
+            print("10/3 = " + d.divide(10,3));
+            print("10/0 = " + d.divide(10,0));
+        } catch(Exception e)  {
+            print(e);
+        }
+    }
+}
---
+++ src/TkJava/TKU20/extract/ExtractInterface.java
@@ -0,0 +1,12 @@
+package TkJava.TKU20.extract;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ExtractInterface {
+    public String value();
+}
